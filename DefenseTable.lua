c62d5e0fe132a7d8009fc2849698050d7184b6fe
Azuredrak0 <122696665+Azuredrak0@users.noreply.github.com>
2023-10-06 20:10:44 -0400

    Update DefenseTable.lua

--- DefenseTable.lua
+++ DefenseTable.lua
@@ -1,12 +1,13 @@
 -- Written By: Azuredrak0 (a.k.a Drokin - Level 60 Warden, Shaman Tank)
--- Code partially based on Avoidance mod
+-- Code partially based on Avoidance mod written by Kip Potter
 
 -- Global version variable...
-DefenseTable_VERSION = "0.9 beta";
+DefenseTable_VERSION = "0.9";
 
 -- Other globals.
-Avoidance_Settings = {};
-Avoidance_salv = {};
+local DefenseTable_Settings = {};
+local DefenseTable_salv = {};
+local targetLVL = 63;
 
 -- Locals.
 local frame;
@@ -14,13 +15,13 @@ local frame;
 --------------------------------------------------------------------------------
 -- Handles loading the add-on.
 --------------------------------------------------------------------------------
-function Avoidance_OnLoad(Self)
-	DEFAULT_CHAT_FRAME:AddMessage(format("Avoidance v%s loaded.", AVOIDANCE_VERSION));
+function DefenseTable_OnLoad(Self)
+	DEFAULT_CHAT_FRAME:AddMessage(format("DefenseTable v%s loaded.", DefenseTable_VERSION));
 	
 	-- Register the game slash commands necessary for our functionality
-	SLASH_AVOIDANCE1 = "/defensetable";
-	SLASH_AVOIDANCE2 = "/dt";
-	SlashCmdList["AVOIDANCE"] = Avoidance_SlashCommand;
+	SLASH_DefenseTable1 = "/defensetable";
+	SLASH_DefenseTable2 = "/dt";
+	SlashCmdList["DefenseTable"] = DefenseTable_SlashCommand;
 	
 	-- Register the events.
 	this:RegisterEvent("VARIABLES_LOADED");
@@ -29,12 +30,13 @@ function Avoidance_OnLoad(Self)
 	this:RegisterEvent("UNIT_DEFENSE");
 	this:RegisterEvent("PLAYER_LOGIN");
 	this:RegisterEvent("PLAYER_AURAS_CHANGED"); 
+	this:RegisterEvent("UNIT_INVENTORY_CHANGED"); 
 end
 
 --------------------------------------------------------------------------------
--- Shows the command (command-line parameters) options for avoidance.
+-- Shows the command (command-line parameters) options for DefenseTable.
 --------------------------------------------------------------------------------
-function Avoidance_ShowCommands()
+function DefenseTable_ShowCommands()
 	DEFAULT_CHAT_FRAME:AddMessage("DefenseTable command options:");
 	if (frame:IsShown()) then
 		DEFAULT_CHAT_FRAME:AddMessage("  show [true] - Shows window.");
@@ -44,13 +46,15 @@ function Avoidance_ShowCommands()
 		DEFAULT_CHAT_FRAME:AddMessage("  hide [true] - Hides window.");
 	end
 	
-	if (Avoidance_Settings.EnableDebugging) then
+	if (DefenseTable_Settings.EnableDebugging) then
 		DEFAULT_CHAT_FRAME:AddMessage("  debugon  [true] - Enables displaying debug information to the chat window.");
 		DEFAULT_CHAT_FRAME:AddMessage("  debugoff [false] - Disables displaying debug information to the chat window.");
 	else
 		DEFAULT_CHAT_FRAME:AddMessage("  debugon  [false] - Enables displaying debug information to the chat window.");
 		DEFAULT_CHAT_FRAME:AddMessage("  debugoff [true] - Disables displaying debug information to the chat window.");
 	end
+	DEFAULT_CHAT_FRAME:AddMessage("  tar60 - set target level to 60 (default).");
+	DEFAULT_CHAT_FRAME:AddMessage("  tar63 - set target level to 63.");
 	DEFAULT_CHAT_FRAME:AddMessage("  ver - Shows the installed version of DefenseTable.");
 	DEFAULT_CHAT_FRAME:AddMessage("  resetframe - Resets the DefenseTable frame to it's default position.");
 	DEFAULT_CHAT_FRAME:AddMessage("  /avoid off will remove savlation when cast on you.");
@@ -58,11 +62,11 @@ function Avoidance_ShowCommands()
 end
 
 --------------------------------------------------------------------------------
--- Handles the slash commands '/avoid' and '/avoidance'.
+-- Handles the slash commands '/dt' and '/DefenseTable'.
 --------------------------------------------------------------------------------
-function Avoidance_SlashCommand(msg)
+function DefenseTable_SlashCommand(msg)
 	if (msg == "ver") then
-		DEFAULT_CHAT_FRAME:AddMessage(format("Avoidance Version: %s", AVOIDANCE_VERSION));
+		DEFAULT_CHAT_FRAME:AddMessage(format("DefenseTable Version: %s", DefenseTable_VERSION));
 	elseif (msg == "show") then
 		if (not frame:IsShown()) then
 			frame:Show();
@@ -71,10 +75,16 @@ function Avoidance_SlashCommand(msg)
 		if (frame:IsShown()) then
 			frame:Hide();
 		end
+	elseif (msg == "tlvl60") then
+		targetLVL = 60;
+		DEFAULT_CHAT_FRAME:AddMessage(format("Target level set to 60."));
+	elseif (msg == "tlvl63") then
+		targetLVL = 63;
+		DEFAULT_CHAT_FRAME:AddMessage(format("Target level set to 63 (Raid bosses)."));
 	elseif (msg == "debugon") then
-		Avoidance_Settings.EnableDebugging = true;
+		DefenseTable_Settings.EnableDebugging = true;
 	elseif (msg == "debugoff") then
-		Avoidance_Settings.EnableDebugging = false;
+		DefenseTable_Settings.EnableDebugging = false;
 	elseif (msg == "resetframe") then
 		frame:ClearAllPoints();
 		frame:SetPoint("CENTER", WorldFrame, 0, 0);
@@ -86,26 +96,27 @@ function Avoidance_SlashCommand(msg)
 		Avoidance_salv = 0
 		DEFAULT_CHAT_FRAME:AddMessage("salvation will NOT be removed");
 	else
-		Avoidance_ShowCommands();
+		DefenseTable_ShowCommands();
 	end
 end
 
 --------------------------------------------------------------------------------
--- Shows the avoidance breakdown.
+-- Shows the defensive stats breakdown.
 --------------------------------------------------------------------------------
-function Avoidance_ShowAvoidance(msg)
+function DefenseTable_ShowAvoidance(msg)
+	--targetLVL = GetTargetLevel();
 	local baseDefense,armorDefense=UnitDefense("player");
 	local defenseContrib = (baseDefense + armorDefense - UnitLevel("player") * 5) / 25;
 	local defenseBonus = (baseDefense + armorDefense - UnitLevel("player") * 5) * .04;
 
 	-- Calculate total avoidance.
-	local baseAvoidance = 5 + defenseBonus;
-	local dodge = GetDodgeChance();
-	local parry = GetParryChance();
+	local baseAvoidance = 5 + defenseBonus+((UnitLevel("player")-targetLVL)*.02);
+	local dodge = GetDodgeChance()+((UnitLevel("player")-targetLVL)*.02);
+	local parry = GetParryChance()+((UnitLevel("player")-targetLVL)*.02);
 	local totalAvoidance = baseAvoidance + dodge + parry;
 
 	-- Calculate mitigation stats
-	local block = GetBlockChance();
+	local block = GetBlockChance()+((UnitLevel("player")-targetLVL)*.02);
 	local playerLevel = UnitLevel("player");
 	local base, effectiveArmor = UnitArmor("player");
 	local armorReduction = effectiveArmor/((85 * playerLevel) + 400);
@@ -115,7 +126,10 @@ function Avoidance_ShowAvoidance(msg)
 
 	-- Calculate chance to be hit
 	local crush = 0;
-	local crit = 5 - defenseBonus;
+	if targetLVL == 63 then
+		crush = 15
+	end
+	local crit = 5 - defenseBonus-((UnitLevel("player")-targetLVL)*.02);
 	if crit <= 0 then
 		crit = 0
 	end		
@@ -128,16 +142,16 @@ function Avoidance_ShowAvoidance(msg)
 	end
 
 	if (avoidanceStack + crush) > 100 then
-		crush = math.abs(100 - avoidanceStack - crush)
+		crush = 100 - avoidanceStack;
 		crit = 0;
 		hit = 0;
 	end
 	if (avoidanceStack + crush + crit) > 100 then
-		crit = math.abs(100 - avoidanceStack - crush - crit)
+		crit = 100 - avoidanceStack - crush;
 		hit = 0;
 	end
-	if (avoidanceStack + crusch + crit + hit) > 100 then
-		hit = math.abs(100 - avoidanceStack - crusch - crit - hit)
+	if (avoidanceStack + crush + crit + hit) > 100 then
+		hit = 100 - avoidanceStack - crush - crit;
 	end
 	
 	-- TODO: Check for shield, and if present, add block chance, otherwise don't add block chance.
@@ -147,13 +161,13 @@ function Avoidance_ShowAvoidance(msg)
 	if (msg == "min") then
 		print(format("Total avoidance: %.2f%%", totalAvoidance));
 	else
-		if (Avoidance_Settings.EnableDebugging) then
+		if (DefenseTable_Settings.EnableDebugging) then
 			--print("");			
 			print(format("  Base avoidance : %.2f%%", baseAvoidance));
 			--print(format("  Def. avoidance : %.2f%%", defenseContrib));
 			print(format("  Dodge : %.2f%%", dodge));
 			print(format("  Parry : %.2f%%", parry));
-			print(format("  Block Chance : %.2f%%", block));
+			print(format("  Block : %.2f%%", block));
 			print(format("    crushed : %.2f%%", crush));
 			print(format("    critted : %.2f%%", crit));
 			print(format("    hit : %.2f%%", hit));
@@ -161,32 +175,33 @@ function Avoidance_ShowAvoidance(msg)
 			print(format("Armor: %d(%.1f%%)", effectiveArmor, armorReduction));
 			print(format("HP: %d", maxHealth));
 		end
-		AvoidanceBaseText:SetText(format("%002.2f%% - base avoidance", baseAvoidance));
-		--AvoidanceDefText:SetText(format("%002.2f%% - avoid. from defense", defenseContrib));
-		AvoidanceDodgeText:SetText(format("%002.2f%% - dodge", dodge));
-		AvoidanceParryText:SetText(format("%002.2f%% - parry", parry));
-		AvoidanceBlockText:SetText(format("%002.2f%% - block", block));
-		AvoidanceBlockText:SetText(format("%002.2f%% - crushed", crush));
-		AvoidanceBlockText:SetText(format("%002.2f%% - critted", crit));
-		AvoidanceBlockText:SetText(format("%002.2f%% - hit", hit));
-		AvoidanceTotalText:SetText(format("%002.2f%% - Dodge + Parry", totalAvoidance));
-		AvoidanceArmorText:SetText(format("%d(%002.1f%%) - armor", effectiveArmor,armorReduction));
-		AvoidanceHPText:SetText(format("%d - MAX HP", maxHealth));
+		DefenseTableTitle:SetText(format("L%d - Defense Table breakdown", targetLVL));
+		DefenseTableBaseText:SetText(format("%002.2f%% - base avoidance", baseAvoidance));
+		--DefenseTableDefText:SetText(format("%002.2f%% - avoid. from defense", defenseContrib));
+		DefenseTableDodgeText:SetText(format("%002.2f%% - dodge", dodge));
+		DefenseTableParryText:SetText(format("%002.2f%% - parry", parry));
+		DefenseTableBlockText:SetText(format("%002.2f%% - block", block));
+		DefenseTableCrushText:SetText(format("   %002.2f%% - crushed", crush));
+		DefenseTableCritText:SetText(format("   %002.2f%% - critted", crit));
+		DefenseTableHitText:SetText(format("   %002.2f%% - hit", hit));
+		DefenseTableTotalText:SetText(format("%002.2f%% - base + Dodge + Parry", totalAvoidance));
+		DefenseTableArmorText:SetText(format("%d(%002.1f%%) - armor", effectiveArmor,armorReduction));
+		DefenseTableHPText:SetText(format("%d - MAX HP", maxHealth));
 	end
 end
 
 --------------------------------------------------------------------------------
 -- Creates the Avoidance frame.
 --------------------------------------------------------------------------------
-function Avoidance_CreateFrame()
+function DefenseTable_CreateFrame()
 	if (frame == nil) then
-		frame = CreateFrame("Button", "AvoidanceFrame", UIParent);
+		frame = CreateFrame("Button", "DefenseTableFrame", UIParent);
 	end
 	
-	if (Avoidance_Settings.Pos == nil) then
+	if (DefenseTable_Settings.Pos == nil) then
 		frame:SetPoint("CENTER", 0, 200);
 	else
-		frame:SetPoint(Avoidance_Settings.Pos, Avoidance_Settings.PosX, Avoidance_Settings.PosY);
+		frame:SetPoint(DefenseTable_Settings.Pos, DefenseTable_Settings.PosX, DefenseTable_Settings.PosY);
 	end
 	
 	frame:SetHeight(100);
@@ -206,14 +221,14 @@ function Avoidance_CreateFrame()
 	-- Title top.
 	local y = -6;
 	
-	AvoidanceTitle = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceTitle:SetPoint("TOPLEFT", 10, y);
-	AvoidanceTitle:SetText("L60 - Defense Table breakdown");
+	DefenseTableTitle = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableTitle:SetPoint("TOPLEFT", 10, y);
+	DefenseTableTitle:SetText("L0(00.00%) - Defense Table breakdown");
 	
 	y = y - 13;
-	AvoidanceBaseText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceBaseText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceBaseText:SetText("00.00% - base avoidance");
+	DefenseTableBaseText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableBaseText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableBaseText:SetText("00.00% - base avoidance");
 	
 	--y = y - 13;
 	--AvoidanceDefText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
@@ -221,54 +236,54 @@ function Avoidance_CreateFrame()
 	--AvoidanceDefText:SetText("00.00% - avoid. from defense");
 	
 	y = y - 13;
-	AvoidanceDodgeText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceDodgeText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceDodgeText:SetText("00.00% - dodge");
+	DefenseTableDodgeText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableDodgeText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableDodgeText:SetText("00.00% - dodge");
 	
 	y = y - 13;
-	AvoidanceParryText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceParryText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceParryText:SetText("00.00% - parry");
+	DefenseTableParryText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableParryText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableParryText:SetText("00.00% - parry");
 	
 	y = y - 13;
-	AvoidanceBlockText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceBlockText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceBlockText:SetText("00.00% - block");
+	DefenseTableBlockText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableBlockText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableBlockText:SetText("00.00% - block");
 
 	y = y - 13;
-	AvoidanceBlockText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceBlockText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceBlockText:SetText("   00.00% - crushed");
+	DefenseTableCrushText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableCrushText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableCrushText:SetText("   00.00% - crushed");
 
 	y = y - 13;
-	AvoidanceBlockText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceBlockText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceBlockText:SetText("   00.00% - critted");
+	DefenseTableCritText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableCritText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableCritText:SetText("   00.00% - critted");
 
 	y = y - 13;
-	AvoidanceBlockText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceBlockText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceBlockText:SetText("   00.00% - hit");
+	DefenseTableHitText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableHitText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableHitText:SetText("   00.00% - hit");
 	
 	y = y - 13;
-	AvoidanceDashesText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceDashesText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceDashesText:SetText("-------------------------");
+	DefenseTableDashesText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableDashesText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableDashesText:SetText("-------------------------");
 	
 	y = y - 13;
-	AvoidanceTotalText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceTotalText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceTotalText:SetText("00.00% - Dodge + Parry");
+	DefenseTableTotalText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableTotalText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableTotalText:SetText("00.00% - base + Dodge + Parry");
 		
 	y = y - 13;
-	AvoidanceArmorText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceArmorText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceArmorText:SetText("0(00.00%) - armor");
+	DefenseTableArmorText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableArmorText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableArmorText:SetText("0(00.00%) - armor");
 
 	y = y - 13;
-	AvoidanceHPText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
-	AvoidanceHPText:SetPoint("TOPLEFT", 10, y);
-	AvoidanceHPText:SetText("0(00.00%) - HP");
+	DefenseTableHPText = frame:CreateFontString(nil, nil, "GameFontNormalSmall");
+	DefenseTableHPText:SetPoint("TOPLEFT", 10, y);
+	DefenseTableHPText:SetText("0(00.00%) - HP");
 	
 	y = y - 19;
 	frame:SetHeight(y * -1);
@@ -282,32 +297,34 @@ function Avoidance_CreateFrame()
 	frame:SetScript("OnDragStart", function() frame:StartMoving(); end);
 	frame:SetScript("OnDragStop", function()
 		frame:StopMovingOrSizing()
-		Avoidance_Settings.Pos, Avoidance_Settings.PosX, Avoidance_Settings.PosY = "BOTTOMLEFT",
+		DefenseTable_Settings.Pos, DefenseTable_Settings.PosX, DefenseTable_Settings.PosY = "BOTTOMLEFT",
 		frame:GetLeft(), frame:GetBottom()
 		end
 	);
-	Avoidance_ShowAvoidance(nil);
+	DefenseTable_ShowAvoidance(nil);
 end
 
 --------------------------------------------------------------------------------
 -- Resets avoidance.
---------------------------------------------------------------------------------
-function Avoidance_Reset()
-	Avoidance_ShowAvoidance(nil);
+-----------------------------------------------------------------------
+function DefenseTable_Reset()
+	DefenseTable_ShowAvoidance(nil);
 end
 
 --------------------------------------------------------------------------------
 -- Fires when an event we are listening to is fired.
 --------------------------------------------------------------------------------
-function Avoidance_OnEvent()
+function DefenseTable_OnEvent()
 	if (event == "VARIABLES_LOADED") then
-		Avoidance_CreateFrame();
+		DefenseTable_CreateFrame();
 	elseif (event == "UNIT_STATS") then
-		Avoidance_ShowAvoidance("stats");
+		DefenseTable_ShowAvoidance("stats");		
 	elseif (event == "UNITDEFENSE") then
-		Avoidance_ShowAvoidance("defense");
+		DefenseTable_ShowAvoidance("defense");
+	elseif (event == "UNIT_INVENTORY_CHANGED") then
+		DefenseTable_ShowAvoidance("equip");
 	elseif (event == "UNIT_AURA") then
-		Avoidance_ShowAvoidance("aura");
+		DefenseTable_ShowAvoidance("aura");
 	elseif (event == "PLAYER_LOGIN") then
 			DEFAULT_CHAT_FRAME:AddMessage("use /avoid for commands");
 -- 			hide window on log in and set salv remove to off
@@ -315,18 +332,18 @@ function Avoidance_OnEvent()
 			Avoidance_salv = 0;
 	elseif (event == "PLAYER_AURAS_CHANGED") then
 			if (Avoidance_salv == 1) then
-			Avoid_CancelSalvationBuff();
+			DefenseTable_CancelSalvationBuff();
 			end
 	end
 	
-	if (Avoidance_Show == 0) then
+	if (DefenseTable_Show == 0) then
 		frame:Show();
-		Avoidance_Show = 1;
+		DefenseTable_Show = 1;
 	end
 end
 
 -- salv remover
-function Avoid_CancelSalvationBuff()
+function DefenseTable_CancelSalvationBuff()
 	local buff = {"Spell_Holy_SealOfSalvation", "Spell_Holy_GreaterBlessingofSalvation"}
 	local counter = 0
 	while GetPlayerBuff(counter) >= 0 do
@@ -349,3 +366,16 @@ function Avoid_CancelSalvationBuff()
 	return nil
 end
 
+-- function GetTargetLevel()
+  -- local targetUnit = UnitName("target");
+  -- if (targetUnit == nil) then
+    -- return nil;
+  -- end
+
+  -- local targetLevel = UnitLevel(targetUnit);
+  -- if (targetLevel == nil) then
+    -- return nil;
+  -- end
+
+  -- return targetLevel;
+-- end
